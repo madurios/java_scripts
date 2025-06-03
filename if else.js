8f9a45a07b4410fe82654fe4326109d4adb824c8
maria <madurios90@gmail.com>
2025-06-03 10:36:24 -0300

    if else

---
+++ if else.js
@@ -0,0 +1,201 @@
+let respostaCorreta = 0
+let respostaIncorreta = 0
+
+//Pergunta1
+console.log(&quot;Quem é o cantor(a) que não sai do meu fone?&quot;)
+console.log(&quot;A - lana del rey&quot;)
+console.log(&quot;B - drake&quot;)
+console.log(&quot;C- the weeknd&quot;)
+console.log(&quot;D - charlie brown jr&quot;)
+let pergunta1 = prompt(&quot;Digite a alternativa correta:&quot;)
+if(pergunta1 == &quot;C&quot; || pergunta1 == &quot;the weeknd&quot;){
+console.log(&quot;Alternativa Correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Incorreta:&quot; + respostaIncorreta)
+
+//Perguta2
+console.log(&quot;Qual país eu viajaria?&quot;)
+console.log(&quot;A - londres&quot;)
+console.log(&quot;B - mexico&quot;)
+console.log(&quot;C - orlando&quot;)
+console.log(&quot;D - maldivas&quot;)
+let pergunta2 = prompt(&quot;Digite a alternativa correta: &quot;)
+if(pergunta2 == &quot;D&quot; || pergunta2 == &quot;maldivas&quot;){
+
+console.log(&quot;Alternativa Correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Incorreta:&quot; + respostaIncorreta)
+
+//Pergunta3
+console.log(&quot;Qual meu desenho favorito na disney&quot;)
+console.log(&quot;A - cinderela&quot;)
+console.log(&quot;B - bela adormecida&quot;)
+console.log(&quot;C - bela e a fera&quot;)
+console.log(&quot;D -branca de neve&quot;)
+let pergunta3 = prompt(&quot;Digite a alternativa correta:&quot;)
+if(pergunta3 == &quot;B&quot; || pergunta3 == &quot;bela adormecida&quot;){
+console.log(&quot;Alternativa Correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Incorreta:&quot; + respostaIncorreta)
+
+//Pergunta4
+console.log(&quot;Qusl foi a primeira musica do the weeknd?&quot;)
+console.log(&quot;A - House Of Balloons &quot;)
+console.log(&quot;B - The Party &quot;)
+console.log(&quot;C -After Hours&quot;)
+console.log(&quot;D - What you Need&quot;)
+let pergunta4 = prompt(&quot;Digita a alternativa correta:&quot;)
+if(pergunta4 == &quot;D&quot; || pergunta4 == &quot;What you Need&quot;){
+console.log(&quot;Aletrnativa correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Errada:&quot; + respostaIncorreta)
+//Pergunta5
+console.log(&quot;Qual é o meu carro dos sonhos?&quot;)
+console.log(&quot;A - Ford &quot;)
+console.log(&quot;B - Audi &quot;)
+console.log(&quot;C -Civic&quot;)
+console.log(&quot;D - Tesla&quot;)
+let pergunta5 = prompt(&quot;Digita a alternativa correta:&quot;)
+if(pergunta5 == &quot;C&quot; || pergunta5 == &quot;Civic&quot;){
+console.log(&quot;Aletrnativa correta!&quot;)
+respostaCorreta ++
+}
+
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Errada:&quot; + respostaIncorreta)
+//Pergunta6
+console.log(&quot;Qual a próxima cor de cabelo que vou pintar?&quot;)
+console.log(&quot;A - preto &quot;)
+console.log(&quot;B - Castanho claro&quot;)
+console.log(&quot;C -Chocolate&quot;)
+console.log(&quot;D -Preto azulado &quot;)
+let pergunta6 = prompt(&quot;Digita a alternativa correta:&quot;)
+if(pergunta6 == &quot;A&quot; || pergunta6 == &quot;preto&quot;){
+console.log(&quot;Aletrnativa correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Errada:&quot; + respostaIncorreta)
+//Pergunta7
+console.log(&quot;Qual estação eu prefiro mais?&quot;)
+console.log(&quot;A - Primavera&quot;)
+console.log(&quot;B -Verão &quot;)
+
+console.log(&quot;C -Outono&quot;)
+console.log(&quot;D - Inverno &quot;)
+let pergunta7 = prompt(&quot;Digita a alternativa correta:&quot;)
+if(pergunta7 == &quot;B&quot; || pergunta7 == &quot;Verão &quot;){
+console.log(&quot;Aletrnativa correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Errada:&quot; + respostaIncorreta)
+//Pergunta8
+console.log(&quot;Cor favorita?&quot;)
+console.log(&quot;A - Vermelho&quot;)
+console.log(&quot;B - Azul &quot;)
+console.log(&quot;C - Roxo&quot;)
+console.log(&quot;D - Preto &quot;)
+let pergunta8 = prompt(&quot;Digita a alternativa correta:&quot;)
+if(pergunta8 == &quot;B&quot; || pergunta8 == &quot; Azul &quot;){
+console.log(&quot;Aletrnativa correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Errada:&quot; + respostaIncorreta)
+//Perguata9
+console.log(&quot;Eu prefiro?&quot;)
+console.log(&quot;A - Piscina &quot;)
+console.log(&quot;B - Praia &quot;)
+console.log(&quot;C -Rio&quot;)
+console.log(&quot;D - Cachoeira &quot;)
+let pergunta9 = prompt(&quot;Digita a alternativa correta:&quot;)
+if(pergunta9 == &quot;B&quot; || pergunta9 == &quot; Praia &quot;){
+console.log(&quot;Aletrnativa correta!&quot;)
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Errada:&quot; + respostaIncorreta)
+//Pergunta10
+console.log(&quot;Se a gente fosse assistir um filme, qual tema seria&quot;)
+console.log(&quot;A - Comedia &quot;)
+console.log(&quot;B -Romance&quot;)
+console.log(&quot;C - Drama&quot;)
+console.log(&quot;D - Terror &quot;)
+let pergunta10 = prompt(&quot;Digita a alternativa correta:&quot;)
+if(pergunta10 == &quot;D&quot; || pergunta10 == &quot; Terror&quot;){
+console.log(&quot;Aletrnativa correta!&quot;)
+
+respostaCorreta ++
+}
+else{
+console.log(&quot;Alternativa Errada!&quot;)
+respostaIncorreta ++
+}
+//Exibir Resultado
+console.log(&quot;\n Resumo Final&quot;)
+console.log(&quot;Resposta Correta:&quot; + respostaCorreta)
+console.log(&quot;Resposta Errada:&quot; + respostaIncorreta)
