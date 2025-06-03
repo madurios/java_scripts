b7edffe513e91027e8a36017d100ee99d02f7998
maria <madurios90@gmail.com>
2025-06-03 10:28:52 -0300

    console

---
+++ console.js
@@ -0,0 +1,41 @@
+/*attividade1
+let nome = "Maria"
+console.log(nome)
+//atividade2
+let idade = 17 
+console.log(idade)
+//atividade3
+let aprendendoJavaScript
+console.log(true)
+//atividade4*/
+let nome = "Maria "
+let sobrenome = "Rios "
+
+console.log(nome + sobrenome)
+//atividade5/*
+let num4 = 40
+let num8 = 80 
+console.log(soma = num4 + num8)
+//atividade6/*
+let numero4 = 40
+let numero8 = 80
+
+console.log(numero4 * numero8)
+//atividade7/*
+let cidade = "ferraz de vascelos"
+let habitantes = 179205
+ 
+ console.log("cidade", cidade)
+ console.log("Número de habitantes", habitantes )
+ //atividade8/*
+ let preco = 60
+ console.log("Preco inicial", preco)
+ 
+ preco = 45
+ console.log("Preco alterado", preco)
+ //ativiade9/*
+ let meuValor
+ console.log(meuValor)
+ //atividade10/*
+ let meuNulo = null
+ console.log(meuNulo)
