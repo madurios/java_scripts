9f7d68128c06f532681e9d69ccf46202d84d2047
maria <madurios90@gmail.com>
2025-06-03 10:50:28 -0300

    funcao

---
+++ funcao.js
@@ -0,0 +1,45 @@
+Função
+
+/*function soma( a,b){
+let resultado = a + b
+return resultado
+}
+
+let numero1 = Number(prompt(&quot;Digite um numero:&quot;))
+let numero2 = Number(prompt(&quot;Digite o numero:&quot;))
+let resultado = soma( numero1,numero2)
+console.log(&quot;O resultado é:&quot;, resultado)
+*/
+
+/*function soma( a,b){
+let resultado = a * b
+return resultado
+}
+
+let numero1 = Number(prompt(&quot;Digite um numero:&quot;))
+let numero2 = Number(prompt(&quot;Digite o numero:&quot;))
+let resultado = soma( numero1,numero2)
+console.log(&quot;O resultado é:&quot;, resultado)
+*/
+
+/*function soma( a,b){
+let resultado = a - b
+return resultado
+}
+
+let numero1 = Number(prompt(&quot;Digite um numero:&quot;))
+let numero2 = Number(prompt(&quot;Digite o numero:&quot;))
+let resultado = soma( numero1,numero2)
+console.log(&quot;O resultado é:&quot;, resultado)
+*/
+
+/*function soma( a,b){
+let resultado = a / b
+return resultado
+}
+
+let numero1 = Number(prompt(&quot;Digite um numero:&quot;))
+let numero2 = Number(prompt(&quot;Digite o numero:&quot;))
+let resultado = soma( numero1,numero2)
+console.log(&quot;O resultado é:&quot;, resultado)
+*/
