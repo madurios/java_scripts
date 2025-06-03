606712e87f30484c967ac35d081dd69499d41546
maria <madurios90@gmail.com>
2025-06-03 10:52:14 -0300

    quebra cabeca

---
+++ quebra cabeca.js
@@ -0,0 +1,56 @@
+/*let categoria = &quot;&quot;
+let idade = 8
+
+if (idade &gt;= 60){
+const nome = &quot;Fernanda&quot;
+categoria = &quot;Criança&quot;
+}else if(idade &gt;= 18 &amp;&amp; idade &lt; 60){
+categoria = &quot;adulto&quot;
+}else if (idade &gt;=12 &amp;&amp; idade &lt;= 18 ){
+categoria = &quot;adolente&quot;
+}else{
+categoria = &quot;criança&quot;
+}
+
+console.log( &quot;categoria:&quot; + categoria)/*
+
+/*let idade = 17
+
+if( idade &gt;= 16 &amp;&amp; idade &lt;18){
+console.log(&quot;voto obrigatório&quot;)
+}else if ( idade &gt;=70){
+console.log(&quot;voto faculativo&quot;)
+}else{
+console.log(&quot;não vota &quot;)
+}/*
+
+/*let numero = -7
+
+if(numero &lt;= 0 ){
+console.log(&quot;Negativo&quot;)
+}else if(numero &gt; 0){
+
+console.log(&quot;Positivo&quot;)
+}else{
+console.log(&quot;Zero&quot;)
+}/*
+let combustivel = &quot;Etanol&quot;
+
+if( combustivel === &quot;Gasolina&quot;){
+console.log(&quot;Voce escolheu gasolina&quot;)
+}else if( combustivel === &quot;Álcool&quot;){
+console.log(&quot;Voce escolehu álcool&quot;)
+}else if( combustivel === &quot;Diesel&quot;){
+console.log(&quot;Voce escolheu diesel&quot;)
+}else{
+console.log(&quot;Opção inválida&quot;)
+}
+let velocidade = 110
+
+if(velocidade &lt;= 60){
+console.log( &quot;Dentro do limite&quot;)
+}else if(velocidade &lt;= 80){
+console.log(&quot;Atenção&quot;)
+}else{
+console.log(&quot;Multado&quot;)
+}
