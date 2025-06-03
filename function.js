82eb4cfd3f716ddad09a22bb25bd2064fa41c377
maria <madurios90@gmail.com>
2025-06-03 10:55:48 -0300

    function

---
+++ function.js
@@ -0,0 +1,100 @@
+/*function ola(){
+console.log(&quot;Olá, mundo!&quot;)
+}
+ola()*/
+
+/*function mostrarNome(){
+console.log(&quot;Maria Eduarda Silva Rios&quot;)
+}
+mostrarNome()*/
+
+/*function mostrarIdade(){
+console.log(&quot;Idade 17 anos&quot;)
+}
+mostrarIdade()*/
+
+/*function somarDoisNumeros(){
+console.log(45 + 80)
+}
+somarDoisNumeros()*/
+
+/*function mostrarMensagem(){
+console.log(&quot;Odeio gente meio termo.&quot;)
+}
+mostrarMensagem()*/
+
+/*function mostrarData(){
+console.log(&quot;06/05/2025&quot;)
+}
+mostrarData()*/
+
+/*function mostrarCidade(){
+
+console.log(&quot;Ferraz de vasconcelos&quot;)
+}
+mostrarCidade()*/
+
+/*function saudar(){
+console.log(&quot;Bem - vinda, maria&quot;)
+}
+saudar()*/
+
+/*function somarDoisNumeros(){
+console.log(5 - 7)
+}
+somarDoisNumeros()*/
+
+/*function mostrarAnimalFavorito(){
+console.log(&quot;Pantera negra&quot;)
+}
+mostrarAnimalFavorito()*/
+
+/*function mostrarJavaScript(){
+console.log(&quot;Estou aprendendo JavaScript!&quot;)
+}
+mostrarJavaScript()*/
+/*function exibirNomeeIdade(){
+console.log(&quot; Nome: Maria, Idade: 17 anos&quot;)
+}
+exibirNomeeIdade()*/
+/*function exibirAula(){
+console.log(&quot;Aula de funções iniciada!&quot;)
+}
+exibirAula()*/
+
+/*function mostrarNumero(numero){
+console.log(&quot;Voce digitou o numero &quot;+ numero)
+}
+mostrarNumero(9)*/
+
+/*function exibirCurso(){
+console.log(&quot;Você cursa o TI&quot;)
+}
+exibirCurso()*/
+
+/*function triplo(){
+console.log(9 * 5)
+}
+triplo()*/
+
+/*function simularChamada(nome){
+console.log(&quot;Chamando!&quot; + nome)
+}
+simularChamada(&quot;Maria&quot; )*/
+
+/*function CorFavorita(cor){
+console.log(&quot;Sua cor favorita é:&quot; + cor)
+}
+CorFavorita(&quot;Vermelho e preto&quot;)*/
+
+/*function SeuPaís(país){
+console.log(&quot;Você mora no &quot; + país)
+}
+SeuPaís(&quot;Brasil&quot;)*/
+/*function NumeroDe1a10(){
+
+for(i=1;i &lt;=10;i++){
+console.log(i)
+}
+}
+NumeroDe1a10()*/
